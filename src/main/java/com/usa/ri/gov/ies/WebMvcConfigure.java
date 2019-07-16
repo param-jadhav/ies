f575f6c29cc322eff845a9b35d91dc049e5bfbe3
prameshwar <prameshwar@192.168.0.13>
2019-07-16 10:01:36 +0530

    initial project upload

---
+++ src/main/java/com/usa/ri/gov/ies/WebMvcConfigure.java
@@ -0,0 +1,7 @@
+package com.usa.ri.gov.ies;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+public class WebMvcConfigure  extends WebMvcConfigurerAdapter{
+
+}
